2f61e052c7f392dabd2e7e8f09b8eae91ee137da
tranthihangng <hangygd@gmail.com>
2024-08-30 16:14:40 +0700

    init project

---
+++ src/hocJava/HelloWorld.java
@@ -0,0 +1,12 @@
+package hocJava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("helloww");
+		System.out.println("helloww");
+		System.out.println("helloww");
+		System.out.println("helloww");
+
+	}
+}
